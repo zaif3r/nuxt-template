dc68e1e23502c34567bb0361d05a79976d8a660b
zaifer <zaif3r@pm.me>
2022-12-19 15:19:06 -0500

    add default head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            charset: "utf-8",
+            viewport: "width=device-width, initial-scale=1",
+            title: "My Nuxt App",
+            meta: [
+                { name: "description", content: "My amazing site." },
+            ],
+        },
+    },
     modules: [
         "@nuxt/image-edge",
         "@nuxtjs/tailwindcss",
